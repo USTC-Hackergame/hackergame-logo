6380600a8792f08cf487395d20d5bbc8c9ced19a
Hypercube <hypercube@0x01.me>
2022-10-24 00:00:00 +0000

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Hackergame logo
+
+## 制作不同年份 logo 的方法
+
+1. 安装 Inkscape。
+1. 安装字体 `BAUHS93.TTF`。
+1. 修改并运行 `build.sh`。
---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+for year in $(seq 2022 2031); do
+    sed s/YEAR/$year/ template.svg | inkscape --pipe --export-plain-svg --export-text-to-path --export-filename=outputs/$year.svg
+done
